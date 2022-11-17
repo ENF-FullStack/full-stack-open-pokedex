3401a5b50bbe8a54f3173967f4529be126ddc59c
VP Tervonen <elamshin@protonmail.com>
2022-11-18 00:17:21 +0200

    11.12 added curl install to sh

--- health_check.sh
+++ health_check.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+apt-get update && apt-get install curl -y
+wait
+
 res=$(curl -s https://white-sky-5236.fly.dev/health)
 
-if [ "$res" == "ok" ]; then
+if [ "$res" = "ok" ]; then
   echo "Succeeded to curl to /health"
   exit 0
 fi
