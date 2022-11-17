aaa39c41995e2a6d854ea0b2ba3ab09e4fab5d3f
VP Tervonen <elamshin@protonmail.com>
2022-11-17 08:09:16 +0200

    11.11 alustava

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ const PORT = process.env.PORT || 8080
 
 app.use(express.static('dist'))
 
+app.get('/health' , (req, res) => {
+  throw 'error...'
+  // eslint-disable-next-line no-unreachable
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('1.0.0')
+})
+
 // run server
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
